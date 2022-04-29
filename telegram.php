e3472a1807b6957b535f72ce721f105c87b8a974
vaduxa18 <93349621+vaduxa18@users.noreply.github.com>
2022-04-29 13:03:28 +0300

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -20,9 +20,5 @@ foreach($arr as $key => $value) {
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-if ($sendToTelegram) {
-  header('Location: thank-you.html');
-} else {
-  header('Location: thank-you.html');
-}
+
 ?>
