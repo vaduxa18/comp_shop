72fd89b58ecc8187ef2db0ef8818ef3e7db2dcbd
vaduxa18 <93349621+vaduxa18@users.noreply.github.com>
2022-04-29 12:39:52 +0300

    create

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot5342472775:AAGEzrh4zDazAizhnJdPLxIMbtVqA7jzu9c/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$token = "5213577847:AAEwG2iv8JgNezjngad9zSozO8mPTS_BOz8";
+$chat_id = "-631759509";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
